49c15613d5d63577b59c7b98e4d3d4eb679462d1
fanxin06011 <fanxin0601@sjtu.edu.cn>
2016-05-24 20:46:25 +0800

    php2

--- chooseData.php
+++ chooseData.php
@@ -12,25 +12,25 @@ $idlist=$_POST["idlist"];
 $idflag=$_POST["idflag"];
 
 
-$con = mysql_connect('localhost', 'vis2016', 'vis2016');
+$con = mysqli_connect('localhost', 'vis2016', 'vis2016');
 
 if (!$con)
  {
- die("Connection failed: " . mysql_error());
+ die("Connection failed: " . mysqli_connect_error());
  }
-mysql_select_db($con,"vis");
+mysqli_select_db($con,"vis");
 $sql="SELECT ID,FILELEN,SRCPORT,DSTPORT,TIMEINT,VCI1,VPI1,ATM1AALTYPE,SRCID,DSTID FROM data3 WHERE (TIMEINT>=".$timestart." and TIMEINT<= ".$timeend.")";
 if($idflag==1){
 	$sql=$sql." AND (SRCID IN(".$idlist.") OR DSTID IN(".$idlist."))";
 }
 //echo $sql;
-$result =  mysql_unbuffered_query($con,$sql);
+$result =  mysqli_query($con,$sql);
 //echo $result;
 $i=0;
 $res=array();
 
 
-while($row = mysql_fetch_array($result)){
+while($row = mysqli_fetch_array($result)){
 	$res[$i]=array(
 		"ID"=>$row['ID'],
 		"FILELEN"=>$row['FILELEN'],
@@ -50,7 +50,7 @@ $json_string = json_encode($res);
   
 echo $json_string;
 
-mysql_close($con);
+mysqli_close($con);
 
 
 
